262accd0b61ab4241c3300c8e6a4f9e4c18bbca1
srisaicollege <srisaicollegeforwomen@gmail.com>
2026-04-08 13:08:01 +0530

    added backend path

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://college-website-backend-jqmv.onrender.com/",
         changeOrigin: true,
         secure: false
       }
